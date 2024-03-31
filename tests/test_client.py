5c44819a4708d96e6b6f0091fe161d6aa1a03523
Raphaël Valyi <rvalyi@akretion.com>
2024-03-31 02:30:42 -0300

    fix test

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,8 +8,7 @@ from tests.fixtures.cons_stat_serv_v4_00 import ConsStatServ
 from tests.fixtures.nfestatusservico4 import NfeStatusServico4SoapNfeStatusServicoNf
 from tests.fixtures.ret_cons_stat_serv_v4_00 import RetConsStatServ
 
-response = """
-<?xml version="1.0" encoding="utf-8"?>
+response = """<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope
     xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
