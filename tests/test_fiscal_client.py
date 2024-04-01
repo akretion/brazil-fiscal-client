b1892e61baac096f9148d9fd7be642fea42b192e
Raphaël Valyi <rvalyi@akretion.com>
2024-04-01 16:21:35 -0300

    rename to FiscalClient and simplify constructor

--- tests/test_fiscal_client.py
+++ tests/test_fiscal_client.py
@@ -1,9 +1,8 @@
 from unittest import TestCase, mock
 
-from xsdata.formats.dataclass.client import Config
 from xsdata.formats.dataclass.transports import DefaultTransport
 
-from brazil_fiscal_client.soap_client import SoapClient as Client
+from brazil_fiscal_client.fiscal_client import FiscalClient
 from tests.fixtures.cons_stat_serv_v4_00 import ConsStatServ
 from tests.fixtures.nfestatusservico4 import NfeStatusServico4SoapNfeStatusServicoNf
 from tests.fixtures.ret_cons_stat_serv_v4_00 import RetConsStatServ
@@ -29,54 +28,39 @@ response = """<?xml version="1.0" encoding="utf-8"?>
 """
 
 
-class ClientTests(TestCase):
+class FiscalClientTests(TestCase):
     def test__init__(self):
-        config = Config.from_service(
-            NfeStatusServico4SoapNfeStatusServicoNf, transport="foobar"
-        )
-        client = Client(config)
-        self.assertIsInstance(client, Client)
-
-        # TODO FIXME
-        # this seems to be due to the workaround with Client.__slots__ ...
-        # self.assertIs(client.parser.context, client.serializer.context)
-        #
-        # client = Client(config, parser=XmlParser())
-        # self.assertIs(client.parser.context, client.serializer.context)
-        #
-        # client = Client(config, serializer=XmlSerializer())
-        # self.assertIs(client.parser.context, client.serializer.context)
-
-    def test_from_service(self):
-        client = Client.from_service(
-            NfeStatusServico4SoapNfeStatusServicoNf,
-            location="http://testurl.com",
+        client = FiscalClient(
+            ambiente="2",
             uf="41",
             pkcs12_data=b"fake_cert",
             pkcs12_password="123456",
+            server="http://testurl.com",
             fake_certificate=True,
         )
         self.assertEqual(client.uf, "41")
         self.assertEqual(client.pkcs12_data, b"fake_cert")
         self.assertEqual(client.pkcs12_password, "123456")
+        self.assertEqual(client.server, "http://testurl.com")
+        self.assertIs(client.parser.context, client.serializer.context)
 
     @mock.patch.object(DefaultTransport, "post")
     def test_send_with_instance_object(self, mock_post):
         mock_post.return_value = response.encode()
 
-        client = Client.from_service(
-            NfeStatusServico4SoapNfeStatusServicoNf,
-            location="http://testurl.com",
+        client = FiscalClient(
+            ambiente="2",
             uf=41,
             pkcs12_data=b"fake_cert",
             pkcs12_password="123456",
+            server="http://testurl.com",
             fake_certificate=True,
         )
 
         result = client.send(
             NfeStatusServico4SoapNfeStatusServicoNf,
             ConsStatServ(
-                tpAmb="1",
+                tpAmb="2",
                 cUF="41",
                 xServ="STATUS",
                 versao="4.00",
